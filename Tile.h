ebd4e8bb214e92bdccab10f933cf62c9c4aa4b77
theBrainsGD <35380139+theBrainsGD@users.noreply.github.com>
2018-01-28 16:26:36 +0100

    Uploading the Tile's class

---
+++ Tile.h
@@ -0,0 +1,25 @@
+#include "Arduino.h"
+#include "Vars.h"
+
+class Tile{
+  public:
+    Tile(void);  //Konstruktor
+    ~Tile(void); //Destruktor
+    void setWall(int dir, boolean state);
+    boolean getWall(int dir);
+    void setCheckpoint(boolean state);
+    boolean getCheckpoint(void);
+    void setBlack(boolean state);
+    boolean getBlack(void);
+    void setVisited(boolean state);
+    boolean getVisited(void);
+    
+  private:
+    boolean WallNorth;
+    boolean WallSouth;
+    boolean WallEast;
+    boolean WallWest;
+    boolean Checkpoint;
+    boolean black;
+    boolean visited;
+  };
